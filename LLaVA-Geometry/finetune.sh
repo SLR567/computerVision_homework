ad4b28a5ddbea6343564523c2884bce8b6bbd191
Lingran Song <MC35100@um.edu.mo>
2024-04-15 18:14:46 +0800

    Add files via upload

--- LLaVA-Geometry/finetune.sh
+++ LLaVA-Geometry/finetune.sh
@@ -15,17 +15,20 @@
 ################## LLaMA-2 ##################
 
 python llava/train/train.py \
-    --lora_enable True \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --model_name_or_path ./checkpoints/llava-v1.5-7b \
     --version v1 \
     --data_path ./playground/data/GeomVerse/TRAIN/TRAIN_MIX/data_cot.jsonl \
     --image_folder ./playground/data \
     --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-7b-finetune-cot \
+    --output_dir ./checkpoints/llava-v1.5-7b-finetune-cot-lora \
     --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
